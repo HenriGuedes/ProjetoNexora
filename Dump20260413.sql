51e18a0fb4be4380f4cf4c2bae1403fcf5709563
HenriGuedes <guedesxx24@gmail.com>
2026-04-13 21:46:08 -0300

    comandos banco de dados
    Added 'criado_em' timestamp column to 'dados' table.

--- Dump20260413.sql
+++ Dump20260413.sql
@@ -1,54 +1,9 @@
--- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
---
--- Host: localhost    Database: landingpage
--- ------------------------------------------------------
--- Server version	8.0.45
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `dados`
---
-
-DROP TABLE IF EXISTS `dados`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `dados` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `telefone` varchar(25) NOT NULL,
   `mensagem` text,
+  `criado_em` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `dados`
---
-
-LOCK TABLES `dados` WRITE;
-/*!40000 ALTER TABLE `dados` DISABLE KEYS */;
-INSERT INTO `dados` VALUES (1,'Henrique Guedes','henriquegsilvestre@gmail.com','(11) 94072-1027','asdasd'),(2,'Henrique Guedes','henriquegsilvestre@gmail.com','(11) 94072-1027','asdasd'),(3,'Henrique Guedes','henriquegsilvestre@gmail.com','(11) 94072-1034','Muito bem deu tudo certo'),(4,'gabriel','gabrielguedes.mmg@gmail.com','(11) 94072-2222','caramnba cai do predio'),(5,'gabriel','gabrielguedes.mmg@gmail.com','(11) 94072-2222','caramnba cai do predio'),(6,'gabrielle','gabriellemmg@gmail.com','(11) 94072-2234','caramnba cai do predio 2'),(7,'gabrielle','gabriellemmg@gmail.com','(11) 94072-2234','caramnba cai do predio 2'),(8,'gabrielle','henrixx.guedes007@gmail.com.br','(11) 94072-2222','asdweqweqwe');
-/*!40000 ALTER TABLE `dados` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2026-04-13 20:39:10
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
